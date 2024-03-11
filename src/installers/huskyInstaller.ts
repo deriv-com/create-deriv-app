c8baea52765efcc4afa20dc95e6316485abb76d3
adrienne-deriv <103016120+adrienne-deriv@users.noreply.github.com>
2024-03-11 13:07:50 +0800

    Update huskyInstaller.ts

--- src/installers/huskyInstaller.ts
+++ src/installers/huskyInstaller.ts
@@ -8,8 +8,7 @@ export const huskyInstaller = (packagePath: string) => {
             'lint-staged': '^10.4.0',
             husky: '^7.0.0',
             '@commitlint/cli': '^17.1.2',
-            '@commitlint/config-conventional': '^17.1.0',
-            '@commitlint/config-nx-scopes': '^17.0.0',
+            '@commitlint/config-conventional': '^17.1.0'
         },
         scripts: {
             prepare: 'husky install',
